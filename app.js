3b34c7431a36d1a806bc710479067463ebbd901c
john <jgh2102@gmail.com>
2020-09-30 02:39:32 +0900

    made session store database

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 
 const express = require('express');
 const session = require('express-session');
+const store = require('connect-pg-simple');
 
 const app = express();
 
 app.set('view engine', 'pug');
-
-
-
 app.use(session({
+  store: new (store(session))(),
   secret: 'a5d63fc5-17a5-459c-b3ba-6d81792158fc',
   resave: false,
   saveUninitialized: false,
@@ -48,15 +47,24 @@ app.use((req, res, next) => {
 
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Home' });
+  res.render('index', {
+    title: 'Home',
+    history: req.session.history,
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
+  res.render('about', {
+    title: 'About',
+    history: req.session.history,
+  });
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact', { title: 'Contact' });
+  res.render('contact', {
+    title: 'Contact',
+    history: req.session.history,
+  });
 });
 
 const port = 8080;
